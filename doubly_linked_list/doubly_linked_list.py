989ddae36552ffa8b70d8e2f4cb6398a61891360
cpscott1 <thacam18gmail.com>
2020-07-29 19:51:37 -0500

    only one failed test from MVP

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -135,20 +135,15 @@ class DoublyLinkedList:
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        if self.head is None and self.tail is None:
-            return
-        self.length -= 1
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-        elif self.head == node:
-            self.head = node.next
-            node.delete()
-        elif self.tail == node:
-            self.tail = node.prev
-            node.delete()
+        if node.prev:
+            node.prev.next = node.next
         else:
-            node.delete()
+            self.head = node.next
+        if node.next:
+            node.next.prev = node.prev
+        else:
+            self.tail = node.prev
+        self.length -= 1
 
     """Returns the highest value currently in the list"""
     def get_max(self):
